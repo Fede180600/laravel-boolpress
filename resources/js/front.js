49a984e018855736a617ab654c89c9703538a010
Fede180600 <fede180600@libero.it>
2022-07-05 16:08:18 +0200

    import App.vue in front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -18,8 +18,8 @@
  
  // const files = require.context('./', true, /\.vue$/i)
  // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
- 
- Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+import App from './views/App.vue';
  
  /**
   * Next, we will create a fresh Vue application instance and attach it to
@@ -28,6 +28,7 @@
   */
  
  const app = new Vue({
-     el: '#app',
+    el: '#root',
+    render: h => h(App),
  });
  
