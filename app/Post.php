585522b7311ea13e1bc45bbed816c2882cb0aa1a
Fede180600 <fede180600@libero.it>
2022-07-07 16:23:56 +0200

    relation between post and category

--- app/Post.php
+++ app/Post.php
@@ -11,7 +11,11 @@ class Post extends Model
     
     protected $fillable = [
         'title',
-        'content'
+        'content',
+        'slug'
     ];
 
+    public function category() {
+        return $this->belongsTo('App\Category');
+    }
 }
