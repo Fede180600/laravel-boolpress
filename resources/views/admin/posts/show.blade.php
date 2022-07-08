dba50468bae1de432b855b8198c1fed3f7d036d6
Fede180600 <fede180600@libero.it>
2022-07-08 16:25:45 +0200

    print tags in post/show.blade

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -2,15 +2,33 @@
 
 @section('content')
     <h1>{{ $post->title }}</h1>
+
     <small>SLUG: <strong>{{ $post->slug }}</strong></small>
+
     <p>Category: {{ $category ? $category->name : 'NESSUNA CATEGORIA' }}</p>
+
+    <small>
+        TAGS:
+
+        @forelse ($post->tags as $tag)
+            <strong>{{ $tag->name }}{{ $loop->last ? '' : ',' }}</strong>
+        @empty
+            nessun tag
+        @endforelse
+
+    </small>
+
     <p>{{ $post->content }}</p>
+
     <div class="d-flex">
+
         <a class="btn btn-primary mr-2" href="{{ route('admin.posts.edit', ['post'=>$post->id]) }}">MODIFICA</a>
+
         <form action="{{ route('admin.posts.destroy', ['post'=>$post->id]) }}" method="POST">
             @csrf
             @method('DELETE')
             <button class="btn btn-danger">ELIMINA</button>
         </form>
     </div>
+
 @endsection
