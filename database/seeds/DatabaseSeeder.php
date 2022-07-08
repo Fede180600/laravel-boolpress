70fdadf3a6455b765c26cc5456c3115c04bbab41
Fede180600 <fede180600@libero.it>
2022-07-08 15:36:08 +0200

    put category & tag seeders in database seeder calls

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(PostsTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(TagSeeder::class);
     }
 }
