1d57fc8575e08cd030ce7978a3ee9e9e3e91d34e
Fede180600 <fede180600@libero.it>
2022-07-12 17:12:24 +0200

    add and use font awesome for dashboard nav items

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,5 +15,6 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 
 mix.js('resources/js/front.js', 'public/js');
+mix.copy('node_modules/@fortawesome/fontawsome-free/webfonts', 'public/webfonts');
 
 
