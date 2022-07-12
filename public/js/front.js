f7cbfd043dac30d9a9f738a251fe6daec2c2b59f
Fede180600 <fede180600@libero.it>
2022-07-12 13:11:18 +0200

    prin card in AppPosts.vue

--- public/js/front.js
+++ public/js/front.js
@@ -1969,10 +1969,35 @@ var render = function render() {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _c("div");
+  return _c("div", {
+    staticClass: "container"
+  }, [_c("div", {
+    staticClass: "row row-cols-3"
+  }, _vm._l(_vm.posts, function (post) {
+    return _c("div", {
+      key: post.id,
+      staticClass: "col"
+    }, [_vm._m(0, true)]);
+  }), 0)]);
 };
 
-var staticRenderFns = [];
+var staticRenderFns = [function () {
+  var _vm = this,
+      _c = _vm._self._c;
+
+  return _c("div", {
+    staticClass: "card m-2",
+    staticStyle: {
+      width: "18rem"
+    }
+  }, [_c("div", {
+    staticClass: "card-body"
+  }, [_c("h5", {
+    staticClass: "card-title"
+  }, [_vm._v("Card title")]), _vm._v(" "), _c("p", {
+    staticClass: "card-text"
+  }, [_vm._v("Some quick example text to build on the card title and make up the bulk of\r\n                            the\r\n                            card's content.\r\n                        ")])])]);
+}];
 render._withStripped = true;
 
 
--- resources/js/components/AppPosts.vue
+++ resources/js/components/AppPosts.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        
+    <div class="container">
+        <div class="row row-cols-3">
+            <div class="col" v-for="post in posts" :key="post.id">
+                <div class="card m-2" style="width: 18rem;">
+                    <!-- <img src="..." class="card-img-top" alt="..."> -->
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.
+                        </p>
+                        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -21,10 +35,10 @@ export default {
     methods: {
         getPosts() {
             Axios
-            .get("http://127.0.0.1:8000/api/posts")
-            .then((resp) => {
-                this.posts = resp.data.results;
-            });
+                .get("http://127.0.0.1:8000/api/posts")
+                .then((resp) => {
+                    this.posts = resp.data.results;
+                });
         },
     }
 
