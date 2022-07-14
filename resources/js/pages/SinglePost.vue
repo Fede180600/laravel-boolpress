336e151b2c77c6df3cc9dbf49e4352285cb0da08
Fede180600 <fede180600@libero.it>
2022-07-14 15:04:16 +0200

    axios call for post details

---
+++ resources/js/pages/SinglePost.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+import Axios from 'axios';
+
+export default {
+    name: 'SinglePost',
+    data() {
+        return {
+            post: null
+        }
+    },
+    created() {
+        this.getPostDetails();
+    },
+    methods: {
+        getPostDetails() {
+            const slug = this.$route.params.slug;
+            Axios
+            .get(`/api/posts/${slug}`)
+            .then((resp) => {
+                this.post = resp.data.results;
+            })
+        }
+    }
+}
+</script>
+
+<style lang="">
+    
+</style>
